764683dca74a750e440fcf3b1026e9aac5a8c233
Rodrigo Nascimento <a22403960@alunos.ulht.pt>
2026-04-11 16:14:30 +0000

    Setup inicial do projeto com configuração de suporte a imagens no settings, criação da app portfolio, implementação dos modelos base Licenciatura e Docente, e configuração avançada de visualização e filtros no painel de administração

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Licenciatura, Docente
 
-# Register your models here.
+@admin.register(Licenciatura)
+class LicenciaturaAdmin(admin.ModelAdmin):
+    # Visualização da tabela
+    list_display = ('nome', 'sigla', 'duracao_anos', 'ects_total')
+    # Campos de pesquisa
+    search_fields = ('nome', 'sigla')
+    # Filtros laterais
+    list_filter = ('duracao_anos',)
+
+@admin.register(Docente)
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'pagina_pessoal_url')
+    search_fields = ('nome',)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=150)
+    sigla = models.CharField(max_length=10)
+    descricao = models.TextField()
+    duracao_anos = models.PositiveIntegerField()
+    ects_total = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nome
+
+class Docente(models.Model):
+    nome = models.CharField(max_length=100)
+    pagina_pessoal_url = models.URLField()
+    imagem = models.ImageField(upload_to='docentes/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
