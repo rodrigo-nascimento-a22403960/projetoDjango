4078138a1ddfc72ddbd57a08a74932c5e75fe438
Rodrigo Nascimento <a22403960@alunos.ulht.pt>
2026-04-28 09:59:51 +0000

    Templates base.html e cursos.html criados com estrutura HTML5

--- escola/views.py
+++ escola/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Curso
 
 def cursos_view(request):
-    cursos = Curso.objects.all()
+    cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
     return render(request, 'escola/cursos.html', {'cursos': cursos})
